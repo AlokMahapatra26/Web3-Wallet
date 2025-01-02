7a1b54981403cb054e5ccc61d390607032e7a2b1
AlokMahapatra26 <alokmahapatra2604@gmail.com>
2025-01-02 15:58:16 +0530

    deployment error fixes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,8 @@
 import Link from "next/link";
 import React from "react";
-import { Poppins } from "next/font/google";
 
 // fonts
-const poppins = Poppins({
-    weight:'600',
-    subsets:['latin']
-    
-  })
+
 
 const Footer = () => {
   return (
